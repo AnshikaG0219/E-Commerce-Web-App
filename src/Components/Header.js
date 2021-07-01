fa38f00b020ee0d5dbe1eb654ed656d538a3f16e
Anshika Gautam <anshikagautam130@gmail.com>
2021-07-01 20:37:51 +0530

    random changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./css/header.css";
-import Main from './Main';
-import Review from './Review';
+import Main from "./Main";
+import Review from "./Review";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 
 export default function Header() {
   return (
     <>
-    <Main />
-    <Review />
-    <Footer />
+      <Main />
+      <Review />
+      <Footer />
     </>
   );
 }
